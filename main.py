a0faf5d312d6a9464331216fce815566efe1ccd0
1997808 <khanhhuyle22@gmail.com>
2021-08-31 18:20:48 +0700

    command and on_mesage work

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 import discord
 import os
+from discord.ext import commands
 from dotenv import load_dotenv
 
-client = discord.Client()
+# client = discord.Client()
+bot_prefix = '-'
+bot = commands.Bot(command_prefix = bot_prefix)
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-
-  if message.content.startswith('$hello'):
-    await message.channel.send("Hello")
-
-@client.event
-async def on_connect():
+@bot.event
+async def on_ready():
+  # channel = bot.get_channel(882179907608776704)
+  # await channel.send("Just connected!?!")
   print("Bot connected to the server!")
 
-client.run(TOKEN)
+@bot.event
+async def on_message(message):
+  if message.author == bot.user:
+    return
+
+  if message.content.startswith('hello'):
+    await message.channel.send("Hello")
+    return
+
+  if bot.user in message.mentions:
+    await message.channel.send(f'My prefix is {bot_prefix}')
+    return
+
+  await bot.process_commands(message)
+  
+@bot.event
+async def on_member_join(member):
+  await member.create_dm()
+  await member.dm_channel.send(f'Welcom to server {member}!')
+
+@bot.command(name='ping')
+async def ping(ctx):
+  await ctx.send(f'Ping! {bot.latency * 1000} ms')
+
+bot.run(TOKEN)
